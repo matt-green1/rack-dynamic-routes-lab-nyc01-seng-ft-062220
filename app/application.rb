71e3ae4a8a2661c190d4be48d2572893054768c2
matt-green1 <60438349+matt-green1@users.noreply.github.com>
2020-07-11 15:06:59 -0400

    Done.

--- app/application.rb
+++ app/application.rb
@@ -2,61 +2,61 @@
 #get help and go over this confused about what's happpening 
 #in the official solution - on line 15
 
-class Application
+# class Application
 
-    @@items = []
-    def call(env)
-      resp = Rack::Response.new
-      req = Rack::Request.new(env)
+#     @@items = []
+#     def call(env)
+#       resp = Rack::Response.new
+#       req = Rack::Request.new(env)
   
-      if req.path.match(/items/)
-        item_name = req.path.split("/items/").last
-        #what is happening in this line?!
-        if item =@@items.find{|i| i.name == item_name}
-          resp.write item.price
-        else 
-          resp.status = 400
-          resp.write "Item not found"
-        end
-      else
-        resp.status=404
-        resp.write "Route not found"
-      end
-      resp.finish
-    end
+#       if req.path.match(/items/)
+#         item_name = req.path.split("/items/").last
+#         #what is happening in this line?!
+#         if item =@@items.find{|i| i.name == item_name}
+#           resp.write item.price
+#         else 
+#           resp.status = 400
+#           resp.write "Item not found"
+#         end
+#       else
+#         resp.status=404
+#         resp.write "Route not found"
+#       end
+#       resp.finish
+#     end
   
-  end
+#   end
 
 
 
 
-#-----------not sure why my answer didn't work belo -----------
+#-----------not sure why my answer didn't work below -----------
 
 #require 'pry'
 
-# class Application
-#     @@items = []
+class Application
+    @@items = []
     
-#     def call(env)
-#         resp = Rack::Response.new
-#         req = Rack::Request.new(env)
-#         if req.path.match(/items/)
-#             item_name = req.path.split(/items/).last
-#             item_instance = @@items.find{|item|
-#                 item.name = item_name}
-#             if req.path.include?(item_name)
-#                 if @@items.include?(item_instance)
-#                     resp.write item_instance.price
-#                 else
-#                     resp.write "Item not found"
-#                     resp.status = 400
-#                 end
-#             end
+    def call(env)
+        resp = Rack::Response.new
+        req = Rack::Request.new(env)
+        if req.path.match(/items/)
+            item_name = req.path.split("/items/").last
+            item_instance = @@items.find{|item|
+                item.name == item_name}
+            if req.path.include?(item_name)
+                if @@items.include?(item_instance)
+                    resp.write item_instance.price
+                else
+                    resp.write "Item not found"
+                    resp.status = 400
+                end
+            end
 
-#         else
-#             resp.write "Route not found"
-#             resp.status = 404
-#         end
-#         resp.finish
-#     end
-# end
+        else
+            resp.write "Route not found"
+            resp.status = 404
+        end
+        resp.finish
+    end
+end
